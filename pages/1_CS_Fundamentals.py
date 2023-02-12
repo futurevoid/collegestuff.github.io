7fcb958e7ee629048b54679afa21cc308ca550a5
futurevoid <tamersaeed710@gmail.com>
2023-02-12 09:20:15 +0300

    added cs fund links

--- pages/1_CS_Fundamentals.py
+++ pages/1_CS_Fundamentals.py
@@ -44,85 +44,46 @@ st.title("""All useful links for Freshmen FCAI Applied AI""")
 
 subtitle= st.markdown(""" <h2 style = "color:rgb(0,250,154)"> <em>CS Fundamentals</em> </h2> """,unsafe_allow_html=True)
 content = st.markdown("""
-##   
+                      
+##Generations of Computers   
 
-[Basic Electronics For Beginners By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=uXr4lXYjXuU)
+[Generations of Computers](https://youtu.be/FLst_k_eWkE)
 
-## introduction to electronics arabic عربي  
+## Read-Only Memory (ROM)
 
-[Introduction to electronics in Arabic](https://www.youtube.com/watch?v=iptorUBENF8)
+[Read-Only Memory (ROM)](https://youtu.be/8UZvuoIzfFM)
 
-## Ohm's Law  
+## Random Access Memory (RAM)
 
-[Ohm's Law By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=_rSHqvjDksg)
+[Random Access Memory (RAM)](https://www.youtube.com/watch?v=PVad0c2cljo)
 
-[Introduction to circuits and Ohm's law | Circuits | Physics | Khan Academy](https://www.youtube.com/watch?v=F_vLWkkOETI)
+## CPU Cache Explained - What is Cache Memory?
 
-## Voltage Current and Resistance  
+[CPU Cache Explained - What is Cache Memory?](https://www.youtube.com/watch?v=yi0FhRqDJfo)
 
-[Voltage Current and Resistance By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=08YugQce9OA)
+## What is a System Bus?
 
-## Series and Parallel Circuits  
+[What is a System Bus?](https://youtu.be/3gtrJarCmY8)
 
-[Series and Parallel Circuits By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=7mdc-lRrW1c)
+## What is a Source Code?
 
-## Diodes Explained  
+[What is a Source Code?](https://www.youtube.com/watch?v=QFKkOYeB-VY)
 
-[Diode Explained By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=g54vURe47gM)
 
-## Semiconductors  
 
-[Semiconductors By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=ethnHSgVbHs)
 
-## Half wave Rectifiers  
 
-[Half wave Rectifiers By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=joDlqsknn-w)
 
-## Full wave Rectifiers  
 
-[Full wave Rectifiers By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=pb5nzUBehyY)
 
-## Full wave Bridge Rectifiers  
 
-[Full wave Bridge Rectifiers By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=EkHch86UXpY)
 
-## Zener Diodes  
 
-[Zener Diodes By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=mmiHX_IzvDw)
 
-## Clipper Circuits
 
-[Clipper Circuits By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=HegEFPLSbLY)
 
-## Clamper Circuits  
 
-[Clamper Circuits By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=PRzrS6NOyAY)
 
-## Transistors  
 
-[Transistors By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=AcxDiesy-nI)
 
-## Bipolar Junction Transistors (BJT)  
-
-[Bipolar Junction Transistors (BJT) By TheOrganicChemistryTutor](h<https://www.youtube.com/watch?v=VJJuxQk5fTY)
-
-## Voltage Multipliers  
-
-[Voltage Multipliers - Half Wave Voltage Doubler Circuit By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=yfykYXdAUNY)
-
-[Voltage Multipliers - Full Wave Voltage Doubler Circuit By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=iDQRB8DOJE4)
-
-[Voltage Multiplier Circuit Using Diodes and Capacitors By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=litsAzP4oqw)
-
-## Voltage Regulators
-
-[Voltage Regulators By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=iomZgZYN3WY)
-
-## Light Emitting Diodes (LEDs)  
-
-[LEDs - Light Emitting Diodes - Basic Introduction By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=Hl4v1aUFWrs)
-
-##Varactor Diodes  
-
-[Varactor Diodes By TheOrganicChemistryTutor](https://www.youtube.com/watch?v=aXuM3eW1fcQ)
 """,unsafe_allow_html=True)
